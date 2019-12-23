21fafcee1a4a23e8f268e53f6b10b7dbfbb58ec9
saucedbenny <management@circlebeats.com>
2019-12-23 07:42:18 -0500

    Working 100

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 module.exports = async function (fastify) {
     // POST BEAT DATA TO DATABASE
     fastify.post('/beatsFull', async (request,reply)=>{
-        let addUrl = `http://35.188.61.24:80/beats/mp3/${request.body.title}`
+        let addUrl = `http://35.203.87.148:80/beats/mp3/${request.body.title}`
         connection.query( 'insert into trakz (title, producer, bpm, userTag1, userTag2, url, filterTag1, filterTag2,genre) values (?,?,?,?,?,?,?,?,?)',[request.body.title,request.body.producer,request.body.bpm,request.body.userTag1,request.body.userTag2,addUrl,request.body.filterTag1,request.body.filterTag2,request.body.genre], function (err,result) {
             if(err){
                 reply.send(err).code(400)
@@ -25,7 +25,7 @@ module.exports = async function (fastify) {
     fastify.get('/beatsFull', async (request,reply)=>{
         await connection.query('SELECT fid, title, producer, plays, bpm, userTag1, userTag2, filterTag1, filterTag2, url,genre FROM trakz', function (error, results, fields) {
             if (error) {
-                console.log(error)
+
             }else {
                 console.log(results)
                 reply.send(results, fields)
