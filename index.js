e788a752e54e67a466053777ed71e4b76560a964
saucedbenny <management@circlebeats.com>
2019-12-27 09:21:40 -0500

    init snipcart endpoint

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host     : '35.203.0.180',
-    user     : 'poweruser',
-    password : 'admin',
-    database : 'circlebeats'
-});
+
 
 module.exports = async function (fastify) {
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({
+        host     : '35.203.0.180',
+        user     : 'poweruser',
+        password : 'admin',
+        database : 'circlebeats'
+    });
+    const shortid = require('shortid');
     // POST BEAT DATA TO DATABASE
     fastify.post('/beatsFull', async (request,reply)=>{
         let addUrl = `http://35.203.87.148:80/beats/mp3/${request.body.title}`
@@ -27,14 +29,29 @@ module.exports = async function (fastify) {
             if (error) {
 
             }else {
-                console.log(results)
+                console.log(results);
                 reply.send(results, fields)
-                //connection.end();
             }
         });
-    })
+    });
+    //GET SNIPCART DATA FROM DATABASE
+    fastify.get('/snips', async (req,reply)=>{
+
+        let [rows] = await connection.execute('SELECT title FROM trakz');
+        let [rowsC] = await connection.execute('SELECT name,options,type FROM customFields');
+
+
+
+        reply.send({
+            name:`${rows[0].title}`,
+            url: `/`,
+            id: shortid.generate(),
+            price: 30.01,
+            customFields: rowsC
+        })
+    });
     //POST COMPLETED CONTACT US FORMS TO DATABASE
-    fastify.post('/contact', async (request,reply)=>{
+    fastify.post('/contact', (request,reply)=>{
         connection.query( 'insert into contact (title, email, message) values (?,?,?)',[request.body.title,request.body.email,request.body.message], function (err,result) {
             if(err){
                 reply.send(err).code(400)
